38a62e5f862503d1860b4d59469aebf3d9655718
ArwaAbuShah <95049047+ArwaAbuShah@users.noreply.github.com>
2021-12-23 11:58:22 +0200

    Delete blog directory

--- blog/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-
-class BlogPost(models.Model):
-    title = models.TextField()
-    slug = models.SlugField(unique=True)
-    content = models.TextField(null=True, blank=True)
